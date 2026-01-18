64ff6c9ca70512a4de6b5bc58bc060b1fddd0b37
lucas <marcopolo1254rn@gmail.com>
2026-01-18 11:49:15 -0300

    beggining of movies landing page complete, adding searching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import Search from './components/Search.jsx';
+import { useState } from 'react';
 
 const App = () => {
+  const [searchTerm,setSearchTerm] = useState('IM SUPERMAN');
+
   return (
     <main>
       <div className="pattern" />
 
       <div className="wrapper">
         <header>
-          <h1>Find <span className="text-gradient">Movies</span>  You`ll Enjoy more</h1>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span>  You Will Enjoy</h1>
         </header>
-        <p>Search: </p>        
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />    
       </div>     
     </main>
   );
